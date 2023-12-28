5a7a89479d18b2d833081a0b42f93728a4264212
sruthik <sruthik2016@gmail.com>
2023-12-29 02:30:53 +0530

    font storage too added

--- content.js
+++ content.js
@@ -25,62 +25,101 @@ let elements1 = document.getElementsByClassName('DKV0Md');
 let elements2 = document.getElementsByClassName('r025kc');
 let sitenames = document.getElementsByClassName('VuuXrf');
 
-chrome.storage.sync.get(['lastColorOption'], function(result) {
+//Function to change clr to texts
+function changeInTextColor(clr, text) {
+    let classesToRemove = ["NE1", "NE2", "NE3", "NE4", "NE5", "NE6", "NE7", "NE8", "NE9", "NE10", "NE11", "NE12"];
+    for (let i = 0; i < elements1.length; i++) {
+        elements1[i].classList.remove(...classesToRemove);
+        sitenames[i].classList.remove(...classesToRemove);
+        elements1[i].classList.add(clr);
+        sitenames[i].classList.add(clr);
+        elements2[i].style.color = text;
+    }
+}
 
-    function changeInTextColorRefresh(clr, text) {
-        let classesToRemove = ["NE1", "NE2", "NE3", "NE4", "NE5", "NE6", "NE7", "NE8", "NE9", "NE10", "NE11", "NE12"];
-        for (let i = 0; i < elements1.length; i++) {
-            elements1[i].classList.remove(...classesToRemove);
-            sitenames[i].classList.remove(...classesToRemove);
-            elements1[i].classList.add(clr);
-            sitenames[i].classList.add(clr);
-            elements2[i].style.color = text;
+//Function to change font to texts
+function changeTextFamily(family) {
+    for (let i = 0; i < elements1.length; i++) {
+        elements1[i].style.fontFamily = family;
+    }
+    for (let i = 0; i < elements2.length; i++) {
+        elements2[i].style.fontFamily = family;
+    }
+}
+
+// Select the node that will be observed for mutations
+const targetNode = document.body;
+// Options for the observer (which mutations to observe)
+const config = {attributes: true, childList: true, subtree: true};
+
+// Callback function to execute when mutations are observed
+const callback = function (mutationsList, observer) {
+    // Use traditional 'for loops' for IE 11
+    for (let mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+            chrome.storage.sync.get(['lastColorOption'], function (result) {
+
+
+                if (result.lastColorOption && window.location.hostname.includes('google.com')) {
+                    // Apply the last selected color option
+                    if (result.lastColorOption === 'addClass1') {
+                        changeInTextColor('NE1', '#e4ae35');
+
+                    } else if (result.lastColorOption === 'addClass2') {
+                        changeInTextColor('NE2', '#eed59f');
+
+                    } else if (result.lastColorOption === 'addClass3') {
+                        changeInTextColor('NE3', '#dfc7ab');
+
+                    } else if (result.lastColorOption === 'addClass4') {
+                        changeInTextColor('NE4', '#dfc7ab');
+
+                    } else if (result.lastColorOption === 'addClass5') {
+                        changeInTextColor('NE5', '#d0c9be');
+
+                    } else if (result.lastColorOption === 'addClass6') {
+                        changeInTextColor('NE6', '#10F057');
+
+                    } else if (result.lastColorOption === 'addClass7') {
+                        changeInTextColor('NE7', '#10F057');
+
+                    } else if (result.lastColorOption === 'addClass8') {
+                        changeInTextColor('NE8', '#10F057');
+
+                    } else if (result.lastColorOption === 'addClass9') {
+                        changeInTextColor('NE9', '#10F057');
+
+                    } else if (result.lastColorOption === 'addClass10') {
+                        changeInTextColor('NE10', '#10F057');
+
+                    } else if (result.lastColorOption === 'addClass11') {
+                        changeInTextColor('NE11', '#10F057');
+
+                    } else if (result.lastColorOption === 'addClass12') {
+                        changeInTextColor('NE12', '#10F057');
+
+                    }
+                    // Add more conditions for other color options
+                }
+            });
+            chrome.storage.sync.get(['lastFontOption'], function (result) {
+
+
+                if (result.lastFontOption && window.location.hostname.includes('google.com')) {
+                    // Apply the last selected font option
+                    changeTextFamily(result.lastFontOption);
+                    // Add more conditions for other color options
+                }
+            });
         }
     }
+};
 
-    if (result.lastColorOption && window.location.hostname.includes('google.com')) {
-        // Apply the last selected color option
-        if (result.lastColorOption === 'addClass1') {
-            changeInTextColorRefresh('NE1', '#e4ae35');
-            
-        } else if (result.lastColorOption === 'addClass2') {
-            changeInTextColorRefresh('NE2', '#eed59f');
-        
-        } else if (result.lastColorOption === 'addClass3') {
-            changeInTextColorRefresh('NE3', '#dfc7ab');
-        
-        } else if (result.lastColorOption === 'addClass4') {
-            changeInTextColorRefresh('NE4', '#dfc7ab');
-        
-        } else if (result.lastColorOption === 'addClass5') {
-            changeInTextColorRefresh('NE5', '#d0c9be');
-        
-        } else if (result.lastColorOption === 'addClass6') {
-            changeInTextColorRefresh('NE6', '#10F057');
-        
-        } else if (result.lastColorOption === 'addClass7') {
-            changeInTextColorRefresh('NE7', '#10F057');
-        
-        } else if (result.lastColorOption === 'addClass8') {
-            changeInTextColorRefresh('NE8', '#10F057');
-        
-        } else if (result.lastColorOption === 'addClass9') {
-            changeInTextColorRefresh('NE9', '#10F057');
-        
-        } else if (result.lastColorOption === 'addClass10') {
-            changeInTextColorRefresh('NE10', '#10F057');
-        
-        } else if (result.lastColorOption === 'addClass11') {
-            changeInTextColorRefresh('NE11', '#10F057');
-        
-        } else if (result.lastColorOption === 'addClass12') {
-            changeInTextColorRefresh('NE12', '#10F057');
-        
-        }
-        // Add more conditions for other color options
-    }
-});
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(callback);
 
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
 
 chrome.runtime.onMessage.addListener(function (request) {
     if (request.task === "apply") {
@@ -100,25 +139,6 @@ chrome.runtime.onMessage.addListener(function (request) {
     let googleSearchBg = document.getElementsByClassName('yuRUbf');
     let googleSearchBgSmall = document.getElementsByClassName('Hdw6tb');
 
-    function changeInTextColor(clr, text) {
-        let classesToRemove = ["NE1", "NE2", "NE3", "NE4", "NE5", "NE6", "NE7", "NE8", "NE9", "NE10", "NE11", "NE12"];
-        for (let i = 0; i < elements1.length; i++) {
-            elements1[i].classList.remove(...classesToRemove);
-            sitenames[i].classList.remove(...classesToRemove);
-            elements1[i].classList.add(clr);
-            sitenames[i].classList.add(clr);
-            elements2[i].style.color = text;
-        }
-    }
-
-    function changeTextFamily(family) {
-        for (let i = 0; i < elements1.length; i++) {
-            elements1[i].style.fontFamily = family;
-        }
-        for (let i = 0; i < elements2.length; i++) {
-            elements2[i].style.fontFamily = family;
-        }
-    }
 
     if (request.task == "addClass1") {
         // array of links to be injected
@@ -129,16 +149,16 @@ chrome.runtime.onMessage.addListener(function (request) {
 
     } else if (request.task == 'addClass3') {
         changeInTextColor('NE3', '#dfc7ab');
-        
+
     } else if (request.task == 'addClass4') {
         changeInTextColor('NE4', '#dfc7ab');
-    
+
     } else if (request.task == 'addClass5') {
         changeInTextColor('NE5', '#d0c9be');
-        
+
     } else if (request.task == 'addClass11') {
         changeInTextColor('NE11', '#10F057');
-    
+
     } else if (request.task == 'addClass6') {
         changeInTextColor('NE6', '#10F057');
 
@@ -162,19 +182,19 @@ chrome.runtime.onMessage.addListener(function (request) {
 
     } else if (request.task === 'f2') {
         changeTextFamily('JetBrains Mono, monospace');
-        
+
     } else if (request.task === 'f3') {
         changeTextFamily('Montserrat, sans-serif');
-    
+
     } else if (request.task === 'f4') {
         changeTextFamily('cursive');
-        
+
     } else if (request.task === 'f5') {
         changeTextFamily('Rosario, sans-serif');
-        
+
     } else if (request.task === 'f6') {
         changeTextFamily('Macondo, cursive');
-        
+
     }
 
 // bg add
--- popup.js
+++ popup.js
@@ -17,9 +17,7 @@ document.getElementById('palette1').addEventListener('click', function() {
         chrome.tabs.sendMessage(tabs[0].id, {task: "addClass1"});
     });
     // Store the last selected color option
-    chrome.storage.sync.set({lastColorOption: 'addClass1'}, function() {
-        console.log('Last color option saved as addClass1');
-    });
+    chrome.storage.sync.set({lastColorOption: 'addClass1'});
 });
 
 document.getElementById('palette2').addEventListener('click', function() {
@@ -33,58 +31,57 @@ document.getElementById('palette3').addEventListener('click', function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {task: "addClass3"});
     });
+    chrome.storage.sync.set({lastColorOption: 'addClass3'});
 });
 
 document.getElementById('palette4').addEventListener('click', function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {task: "addClass4"});
     });
+    chrome.storage.sync.set({lastColorOption: 'addClass4'});
 });
 
 document.getElementById('palette5').addEventListener('click', function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {task: "addClass5"});
     });
+    chrome.storage.sync.set({lastColorOption: 'addClass5'});
 });
 
 document.getElementById('palette11').addEventListener('click', function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {task: "addClass11"});
     });
+    chrome.storage.sync.set({lastColorOption: 'addClass6'});
 });
 
 //Colors for Light Mode
 
 document.getElementById('palette6').addEventListener('click', function() {
-    console.log("elements[i].classList");
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {task: "addClass6"});
     });
 });
 
 document.getElementById('palette7').addEventListener('click', function() {
-    console.log("elements[i].classList");
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {task: "addClass7"});
     });
 });
 
 document.getElementById('palette8').addEventListener('click', function() {
-    console.log("elements[i].classList");
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {task: "addClass8"});
     });
 });
 
 document.getElementById('palette9').addEventListener('click', function() {
-    console.log("elements[i].classList");
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {task: "addClass9"});
     });
 });
 
 document.getElementById('palette10').addEventListener('click', function() {
-    console.log("elements[i].classList");
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {task: "addClass10"});
     });
@@ -96,36 +93,42 @@ document.getElementById('font1').addEventListener('click', function(tabs) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {task: "f1"});
     });
+    chrome.storage.sync.set({lastFontOption: 'Inter, cursive'});
 })
 
 document.getElementById('font2').addEventListener('click', function(tabs) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {task: "f2"});
     });
+    chrome.storage.sync.set({lastFontOption: 'JetBrains Mono, monospace'});
 })
 
 document.getElementById('font3').addEventListener('click', function(tabs) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {task: "f3"});
     });
+    chrome.storage.sync.set({lastFontOption: 'Montserrat, sans-serif'});
 })
 
 document.getElementById('font4').addEventListener('click', function(tabs) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {task: "f4"});
     });
+    chrome.storage.sync.set({lastFontOption: 'cursive'});
 })
 
 document.getElementById('font5').addEventListener('click', function(tabs) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {task: "f5"});
     });
+    chrome.storage.sync.set({lastFontOption: 'Rosario, sans-serif'});
 })
 
 document.getElementById('font6').addEventListener('click', function(tabs) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {task: "f6"});
     });
+    chrome.storage.sync.set({lastFontOption: 'Macondo, cursive'});
 })
 // apply background
 document.getElementById('bgpalette1').addEventListener('click', function() {
